3a30b582b86a8836c90596e8749b41925b102bd1
Prince Rwamatwara <princeyannick.pr@gmail.com>
2024-07-16 10:54:53 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/io/princesworld/testapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.princesworld.testapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var appTheme = ThemeData(
+
+  fontFamily: GoogleFonts.pacifico().fontFamily,
+
+  bottomAppBarTheme: const BottomAppBarTheme(color: Colors.black87),
+  brightness: Brightness.dark,
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(fontSize: 18),
+    bodyMedium:TextStyle(fontSize: 16),
+
+    labelMedium: TextStyle(
+      letterSpacing: 1.5, 
+      fontWeight: FontWeight.bold,
+    ),
+    displayLarge: TextStyle(fontWeight: FontWeight.bold),
+    titleMedium: TextStyle(color: Colors.grey) 
+  ),
+  buttonTheme: const ButtonThemeData(),
+
+);
